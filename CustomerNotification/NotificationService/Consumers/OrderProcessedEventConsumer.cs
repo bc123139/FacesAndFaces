4ee3fc5e923671ccb63329a232051ec2b2fda03c
Usman Tahir <Usman.tahir@synergy-it.com>
2022-02-21 23:21:46 +0500

    Add notification service

---
+++ CustomerNotification/NotificationService/Consumers/OrderProcessedEventConsumer.cs
@@ -0,0 +1,45 @@
+﻿using MassTransit;
+using Messaging.InterfacesConstants.Events;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationService.Consumers
+{
+    public class OrderProcessedEventConsumer : IConsumer<IOrderProcessedEvent>
+    {
+        public async Task Consume(ConsumeContext<IOrderProcessedEvent> context)
+        {
+            var rootFolder = AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin"));
+            var result = context.Message;
+            var facesData = result.Faces;
+            if (facesData.Count < 1)
+            {
+                await Console.Out.WriteLineAsync($"No faces detected");
+            }
+            else
+            {
+                int j = 0;
+                foreach (var face in facesData)
+                {
+                    MemoryStream ms = new MemoryStream(face);
+                    var image = Image.FromStream(ms);
+                    image.Save(rootFolder + "/Images/face" + j + ".jpg", ImageFormat.Jpeg);
+                    j++;
+                }
+
+            }
+
+            await context.Publish<IOrderDispatchedEvent>(new
+            {
+                context.Message.OrderId,
+                DispatchDataTime = DateTime.UtcNow
+            });
+        }
+    }
+}
---
+++ CustomerNotification/NotificationService/Program.cs
@@ -0,0 +1,49 @@
+﻿using GreenPipes;
+using MassTransit;
+using Messaging.InterfacesConstants.Constants;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NotificationService.Consumers;
+using System;
+using System.Threading.Tasks;
+
+namespace NotificationService
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreatHostBuilder(args).Build();
+            await host.RunAsync();
+        }
+
+        private static IHostBuilder CreatHostBuilder(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args)
+                
+
+                .ConfigureServices((hostContext, services) =>
+                {
+                    
+                    services.AddMassTransit(c =>
+                    {
+                        c.AddConsumer<OrderProcessedEventConsumer>();
+                    });
+                    services.AddSingleton(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+                    {
+                        cfg.Host("localhost", "/", h => { });
+                        cfg.ReceiveEndpoint(RabbitMqMassTransitConstants.NotificationServiceQueue, e =>
+                        {
+                            e.PrefetchCount = 16;
+                            e.UseMessageRetry(x => x.Interval(2, TimeSpan.FromSeconds(10)));
+                            e.Consumer<OrderProcessedEventConsumer>(provider);
+                        });
+                        //cfg.ConfigureEndpoints(provider);
+                    }));
+                    services.AddSingleton<IHostedService, BusService>();
+
+                });
+            return hostBuilder;
+        }
+    }
+}
